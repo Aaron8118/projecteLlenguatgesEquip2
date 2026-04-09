be474469818cef8fdc4d78569b7298982ee74ae1
Asier-code <asifersan2@alu.edu.gva.es>
2026-04-09 09:12:00 +0200

    cambios y mensaje de bienvenida

---
+++ js/mensaje_paginas.js
@@ -0,0 +1,32 @@
+function inicio() {
+
+    let nombrePagina = document.body.dataset.nombre;
+
+    const div = document.createElement("div");
+
+    div.style.position = "fixed";
+    div.style.top = "80px";
+    div.style.left = "20px";
+    div.style.background = "#1e1e2e";
+    div.style.color = "white";
+    div.style.padding = "15px";
+    div.style.border = "2px solid #55aa55";
+    div.style.fontFamily = "monospace";
+    div.style.zIndex = "9999";
+    div.style.boxShadow = "0 0 10px black";
+
+    div.innerHTML = `
+        <div style="display:flex;justify-content:space-between;align-items:center;">
+            <span>¡Has entrado a ${nombrePagina}!</span>
+            <span style="cursor:pointer;margin-left:10px;">✖</span>
+        </div>
+    `;
+
+    document.body.appendChild(div);
+
+    div.querySelector("span:last-child").onclick = () => {
+        div.remove();
+    };
+}
+
+window.addEventListener("load", inicio);
