7a6a74647a5b1c0853f58440eb56aae85d9bb370
Asier-code <asifersan2@alu.edu.gva.es>
2026-05-22 18:19:44 +0200

    cambios nuevos

--- js/sonido_animal.js
+++ js/sonido_animal.js
@@ -2,31 +2,37 @@ let suma = 0;
 let audioActual = null;
 
 function inicio() {
-    let audios = document.querySelectorAll('audio[id$="_sonido"]');
+    let audios = document.querySelectorAll('so[id$="_sonido"]');
     for (let i = 0; i < audios.length; i++) {
-        audios[i].addEventListener('ended', function() {
-            suma = 0;
-            audioActual = null;
-        });
+        audios[i].addEventListener('ended', finalizarAudio);
     }
-
     let botones = document.querySelectorAll('button[data-sonido]');
     for (let i = 0; i < botones.length; i++) {
-        botones[i].addEventListener('click', function() {
-            reproducirSonido(this.dataset.sonido);
-        });
+        botones[i].addEventListener('click', pulsarBotonSonido);
     }
 }
 
+function finalizarAudio() {
+    suma = 0;
+    audioActual = null;
+}
+
+function pulsarBotonSonido() {
+    reproducirSonido(this.dataset.sonido);
+}
+
+//para reproducir el sonido del animal
 function reproducirSonido(id) {
     if (suma === 1 && audioActual !== null) {
         audioActual.pause();
         audioActual.currentTime = 0;
     }
-    let audio = document.getElementById(id);
-    audio.play();
+
+    let so = document.getElementById(id);
+    so.play();
+
     suma = 1;
-    audioActual = audio;
+    audioActual = so;
 }
 
 window.addEventListener('load', inicio);
