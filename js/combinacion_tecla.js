91121a9486d259f42fc0927a06cd5a44c961594b
Asier-code <asifersan2@alu.edu.gva.es>
2026-06-04 11:53:29 +0200

    control-c en todas

--- js/combinacion_tecla.js
+++ js/combinacion_tecla.js
@@ -1,9 +1,5 @@
-//variables para usar
+//en el body para así poder afectar a todos los body de las paginas
 let body = document.body;
-let contactoBloque = document.getElementsByClassName("contacto");
-let contacto = contactoBloque[0];
-
-//array de colores (pd: equipo no se me ocurren mas en ingles)
 let coloreselec = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'white'];
 function cambiarColor(e) {
     if (e.ctrlKey && e.key.toLowerCase() === "c") {
@@ -11,15 +7,11 @@ function cambiarColor(e) {
         let colorAleatorio = coloreselec[Math.floor(Math.random() * 7)];
         body.style.backgroundColor = colorAleatorio;
         body.style.backgroundImage = "none";
-        contacto.style.backgroundImage = "none";
-        contacto.style.backgroundColor = colorAleatorio;
     }
 
     if (e.key === "Alt") {
         body.style.backgroundColor = "";
         body.style.backgroundImage = "";
-        contacto.style.backgroundImage = "";
-        contacto.style.backgroundColor = "";
     }
 }
 
